f9a51f20847b797ad5aab5b3b8c52a9a534956a2
Drakmyth <shaun.hamman@gmail.com>
2025-04-07 23:53:30 -0400

    Implement reading layout file

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Tile int
+
+const (
+	Tile_Empty Tile = iota
+	Tile_Wall
+	Tile_Brick
+	Tile_OneWay
+)
+
+type Level []Tile
+
+func load_level(path string) (*Level, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return &Level{}, nil
+}
 
 func main() {
-	fmt.Println("Hello world.")
+	_, err := load_level("level23.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
